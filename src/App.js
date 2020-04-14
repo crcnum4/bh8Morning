ebb3435b8186d5607a75176c7bbcb99216d2ff26
Cliff Choiniere <crcnum4@gmail.com>
2020-04-14 11:50:26 -0400

    added CSS to both App.js and post.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to BrainHive!</h1>
-        {this.renderPosts()}
+        <div className="postList">{this.renderPosts()}</div>
       </div>
     );
   }
